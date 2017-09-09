12e4ea382eac662dbb0b50030641a3301ee15aa6
Ristomatti Airo <ristomatti@kallio.io>
2017-09-09 16:22:25 +0300

    Publish only changes above tolerance

--- src/mqtt-analog-controller.js
+++ src/mqtt-analog-controller.js
@@ -1,13 +1,12 @@
 const config = require('config');
 const mqttjs = require('mqtt');
 const linearScale = require('simple-linear-scale');
+const approxEq = require('approximately-equal');
 const { ADCPi } = require('abelectronics');
 
 const analogInputs = [1, 2, 3, 4, 5, 6, 7, 8];
-const interval = 1000; // ms
-
-const mqttConfig = config.get('mqtt');
-const { topic } = mqttConfig;
+const interval = 1000; // milliseconds
+const tolerance = .01; // tolerance for voltage floating
 
 const maxVoltages = {
   '1': 5,
@@ -20,8 +19,13 @@ const maxVoltages = {
   '8': 5
 };
 
+const mqttConfig = config.get('mqtt');
+const { topic } = mqttConfig;
+
 const adc = new ADCPi(0x68, 0x69, 16);
 const mqtt = mqttjs.connect(mqttConfig.url, mqttConfig.options);
+
+const prevVoltage = {};
 
 mqtt.on('connect', () => {
   mqtt.publish(topic.status, 'online', {
@@ -35,20 +39,27 @@ mqtt.on('connect', () => {
 
 function readInputs() {
   resetConsole();
+
   analogInputs.forEach(input => {
     let voltage = round(adc.readVoltage(input));
     let maxVoltage = maxVoltages[input];
     let toPercentage = linearScale([0, maxVoltage], [0, 100], true);
     let value = Math.round(toPercentage(voltage));
 
-    mqtt.publish(`${topic.root}/${input}`, JSON.stringify(value), {
-      qos: 0,
-      retain: false
-    });
-
     console.log(`Input ${input}: ${value}% (${voltage}V)`);
+
+    if (aboveTolerance(input, voltage)) {
+      mqtt.publish(`${topic.root}/${input}`, JSON.stringify(value), {
+        qos: 0,
+        retain: false
+      });
+    }
+    prevVoltage[input] = voltage;
   });
-  console.log();
+}
+
+function aboveTolerance(input, voltage) {
+  return !approxEq(voltage, prevVoltage[input], tolerance);
 }
 
 function round(voltage) {
